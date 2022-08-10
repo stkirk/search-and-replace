b7807fdbfd770c835453e6b95aa1d44052992359
stkirk <shaanekirk@gmail.com>
2022-08-10 15:30:41 -0400

    changed state object name to more semantically reflect its values

--- src/components/SearchAndReplaceForm.js
+++ src/components/SearchAndReplaceForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 const SearchAndReplaceForm = () => {
-  const initialFormValues = {
+  const initialAppState = {
     // may need to set initial file as empty Blob for typing issues
     selectedFile: "",
     search_param: "",
@@ -9,18 +9,18 @@ const SearchAndReplaceForm = () => {
     newFileText: "",
     count: 0,
   };
-  const [formValues, setFormValues] = useState(initialFormValues);
+  const [appState, setAppState] = useState(initialAppState);
 
   const onChange = (e) => {
     if (e.target.name === "selectedFile") {
-      setFormValues({
-        ...formValues,
+      setAppState({
+        ...appState,
         selectedFile: e.target.files[0],
       });
       console.log("selectedFile--> ", e.target.files[0], e.target.name);
     } else {
-      setFormValues({
-        ...formValues,
+      setAppState({
+        ...appState,
         [e.target.name]: e.target.value,
       });
     }
@@ -29,18 +29,18 @@ const SearchAndReplaceForm = () => {
   const submitHandler = (e) => {
     e.preventDefault();
     // pull the text out of the file
-    formValues.selectedFile
+    appState.selectedFile
       .text()
       .then((text) => {
         // build regex for our search pattern, case insentive option add i to flag in regex
-        const regex = new RegExp(formValues.search_param, "g");
+        const regex = new RegExp(appState.search_param, "g");
         // count occurences of search param using regex
         const count = (text.match(regex) || []).length;
         // make the necessary edits and count search terms in the file's text
-        const new_text = text.replace(regex, formValues.replacement_param);
+        const new_text = text.replace(regex, appState.replacement_param);
         // set new_text and count to state
-        setFormValues({
-          ...formValues,
+        setAppState({
+          ...appState,
           newFileText: new_text,
           count: count,
         });
@@ -69,7 +69,7 @@ const SearchAndReplaceForm = () => {
           className="text-input"
           placeholder="Enter a string to search and replace"
           name="search_param"
-          value={formValues.search_param}
+          value={appState.search_param}
           onChange={onChange}
         />
         <input
@@ -77,12 +77,12 @@ const SearchAndReplaceForm = () => {
           className="text-input"
           placeholder="Enter a replacement for the search string"
           name="replacement_param"
-          value={formValues.replacement_param}
+          value={appState.replacement_param}
           onChange={onChange}
         />
         <button className="submit-btn">
-          Search file for {formValues.search_param} and replace with{" "}
-          {formValues.replacement_param}
+          Search file for {appState.search_param} and replace with{" "}
+          {appState.replacement_param}
         </button>
       </form>
     </div>
