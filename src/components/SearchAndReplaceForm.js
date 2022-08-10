32ff53280520864cea801a3660a664cef6788de6
stkirk <shaanekirk@gmail.com>
2022-08-10 13:46:44 -0400

    added inputs for search and replace params, modified onChange handler to accept their values and control with state

--- src/components/SearchAndReplaceForm.js
+++ src/components/SearchAndReplaceForm.js
@@ -9,18 +9,47 @@ const SearchAndReplaceForm = () => {
   };
   const [formValues, setFormValues] = useState(initialFormValues);
 
-  const onFileChange = (e) => {
-    setFormValues({
-      ...formValues,
-      selectedFile: e.target.files[0],
-    });
-    console.log("selectedFile--> ", e.target.files[0]);
+  const onChange = (e) => {
+    if (e.target.name === "selectedFile") {
+      setFormValues({
+        ...formValues,
+        selectedFile: e.target.files[0],
+      });
+      console.log("selectedFile--> ", e.target.files[0], e.target.name);
+    } else {
+      setFormValues({
+        ...formValues,
+        [e.target.name]: e.target.value,
+      });
+      console.log(e.target.value);
+    }
   };
 
   return (
     <div className="form-container">
       <form>
-        <input type="file" name="selectedFile" onChange={onFileChange} />
+        <input
+          className="file-btn"
+          type="file"
+          name="selectedFile"
+          onChange={onChange}
+        />
+        <input
+          type="text"
+          className="text-input"
+          placeholder="Enter a string to search and replace"
+          name="search_param"
+          value={formValues.search_param}
+          onChange={onChange}
+        />
+        <input
+          type="text"
+          className="text-input"
+          placeholder="Enter a replacement for the search string"
+          name="replacement_param"
+          value={formValues.replacement_param}
+          onChange={onChange}
+        />
       </form>
     </div>
   );
